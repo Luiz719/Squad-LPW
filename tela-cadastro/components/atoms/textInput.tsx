a7ba6351c603c907691bb7359ce39cd1ea3ef1c9
taynavlv <88676777+taynavlv@users.noreply.github.com>
2025-10-21 14:51:38 -0300

    Update and rename inputField.tsx to textInput.tsx

--- tela-cadastro/components/atoms/textInput.tsx
+++ tela-cadastro/components/atoms/textInput.tsx
@@ -4,6 +4,8 @@ interface InputFieldProps {
     type?: string;
     placeholder?: string;
     value: string;
+    id: string;
+    name: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     error?: string;
 }
@@ -12,6 +14,8 @@ export const InputField: React.FC<InputFieldProps> = ({
     type = "text",
     placeholder,
     value,
+    id,
+    name,
     onChange,
     error,
 }) => {
@@ -21,6 +25,8 @@ export const InputField: React.FC<InputFieldProps> = ({
                 type={type}
                 placeholder={placeholder}
                 value={value}
+                id={id}
+                name={name}
                 onChange={onChange}
                 className={`border rounded-xl px-4 py-2 text-sm outline-none transition-all
                     ${error ? "border-red-500" : "border-gray-300 focus:border-blue-500"}
