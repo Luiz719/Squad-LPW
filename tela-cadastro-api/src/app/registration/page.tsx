af68f991ea477c6e8f3db144e0ef1addad35db5d
Luiz Gonzaga <roinuj-01@outlook.com>
2025-12-18 01:40:42 -0300

    removed issues

--- tela-cadastro-api/src/app/registration/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { RegistrationTemplate } from "../../../components/templates/RegistrationTemplate"
-
-export default function RegisterPage() {
-  return (
-    <RegistrationTemplate />
-  );
-}
